b9684f2464f03e61fea9ffd9b94a313f71e55e50
bm20050 <bm20050@gmail.com>
2023-04-18 16:17:15 +0900

    04.18 update

---
+++ Chapter06TH_FULL/src/main/java/edu/pnu/Chapter06ThFullApplication.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter06ThFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter06ThFullApplication.class, args);
+	}
+
+}
